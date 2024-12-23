fc9da0c7204144e13893ac00b48b8098ab578496
murodbek_08 <murodbekarslonov201@gmail.com>
2024-12-23 12:28:59 +0500

    new pages

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ owl - carousel - 1; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 $(".owl-carousel-1").owlCarousel({
   loop: true,
   margin: 40,
   nav: true,
   autoplay: true,
-  autoplayTimeout: 1000,
+  autoplayTimeout: 1300,
   autoplayHoverPause: true,
   responsive: {
     0: {
@@ -17,13 +18,13 @@ $(".owl-carousel-1").owlCarousel({
     },
   },
 });
-
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ owl - carousel - 2; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 $(".owl-carousel-2").owlCarousel({
   loop: true,
   margin: 40,
   nav: true,
+  autoplayTimeout: 1300,
   autoplay: true,
-  autoplayTimeout: 1000,
   autoplayHoverPause: true,
   responsive: {
     0: {
@@ -37,29 +38,29 @@ $(".owl-carousel-2").owlCarousel({
     },
   },
 });
-
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ owl - carousel - 3; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 $(".owl-carousel-3").owlCarousel({
   loop: true,
   nav: false,
   autoplay: true,
-  autoplayTimeout: 1000,
+  autoplayTimeout: 1300,
   autoplayHoverPause: true,
   responsive: {
     0: {
-      items: 1.1,
+      items: 1,
     },
     400: {
-      items: 1.2,
+      items: 1,
     },
     600: {
-      items: 2.1,
+      items: 2,
     },
     1000: {
       items: 2.4,
     },
   },
 });
-
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ button full cards view more @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 document.addEventListener("DOMContentLoaded", () => {
   const buttonCard = document.getElementById("button");
   const buttonCards = document.getElementById("buttons");
@@ -68,3 +69,13 @@ document.addEventListener("DOMContentLoaded", () => {
     buttonCards.classList.toggle("cards");
   });
 });
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ loading @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
+const loading = document.getElementById("loading");
+
+const loadingDuration = 3000; // 1.8s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ aos @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
+AOS.init();
