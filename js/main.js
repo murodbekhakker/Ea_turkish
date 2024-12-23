9f111e59a64eb228772c7acb8a17ef26e03c6f59
murodbek_08 <murodbekarslonov201@gmail.com>
2024-12-23 16:07:23 +0500

    new skroll

--- js/main.js
+++ js/main.js
@@ -79,3 +79,27 @@ setTimeout(() => {
 }, loadingDuration);
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ aos @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
 AOS.init();
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@ bac to top @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@//
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+const myBtn = document.getElementById("myBtn");
+myBtn.onclick = function () {
+  const scrollStep = -window.scrollY / (10 / 180);
+  const scrollInterval = setInterval(() => {
+    if (window.scrollY === 0) {
+      clearInterval(scrollInterval);
+    } else {
+      window.scrollBy(0, scrollStep);
+    }
+  }, 15);
+};
